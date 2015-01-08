c89a56f10dfbaed9ff2aa026bed242ed752c7001
lfujii <lfujii@lfujii.com>
2015-01-08 10:58:28 -1000

    Initial Commit

---
+++ TestGitHub1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitHub1
+//
+//  Created by Layton Fujii on 1/8/15.
+//  Copyright (c) 2015 lfujii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
